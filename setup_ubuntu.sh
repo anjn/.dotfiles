9422b6ed4dafe9821886d9f61a904cdf7792a229
Jun ANDO <juna@xilinx.com>
2021-05-07 21:01:04 +0900

    Updated for Ubuntu 20.04

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -67,7 +67,7 @@ function append_if_not_included {
 }
 
 # Git
-apt_install git
+apt_install git curl
 
 # .dotfiles
 if [[ ! -e ~/.dotfiles ]] ; then
@@ -76,71 +76,9 @@ if [[ ! -e ~/.dotfiles ]] ; then
   popd
 fi
 
-## Regolith Linux
-if not_installed regolith-desktop ; then
-  sudo add-apt-repository -y ppa://kgilmer/regolith-stable
-  sudo apt update
-  sudo apt install -y regolith-desktop
-
-  link_file .Xresources.d
-  link_file .Xresources-regolith
-  link_file .config/regolith
-
-  # Replace backend: https://github.com/chjj/compton/issues/152
-  sudo sed -i 's/backend = "glx"/backend = "xrender"/' /etc/regolith/compton/config
-fi
-
-apt_install gnome-icon-theme
-
-apt_install tree
-apt_install build-essential valgrind
-apt_install htop keepass2 fio hddtemp smartmontools
-
-# Google Chrome
-if not_installed google-chrome-stable ; then
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  sudo apt install -y ./google-chrome-stable_current_amd64.deb
-fi
-
-# Google drive
-if not_installed google-drive-ocamlfuse ; then
-  sudo add-apt-repository -y ppa:alessandro-strada/ppa
-  sudo apt update
-  sudo apt install -y google-drive-ocamlfuse
-fi
-
-if [[ ! -e ~/GoogleDrive ]] ; then
-  google-drive-ocamlfuse
-  mkdir ~GoogleDrive
-fi
-append_if_not_included $HOME/.bash_profile "google-drive-ocamlfuse GoogleDrive"
-
-# Linuxbrew
-if not_installed linuxbrew-wrapper ; then
-  sudo apt install -y linuxbrew-wrapper
-  brew
-  append_if_not_included $HOME/.bash_profile 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"'
-  append_if_not_included $HOME/.bash_profile 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"'
-  append_if_not_included $HOME/.bash_profile 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"'
-fi
-
-if not_installed steam-launcher ; then
-  wget https://repo.steampowered.com/steam/archive/precise/steam_latest.deb
-  sudo apt install -y ./steam_latest.deb
-fi
-
-# Fonts
-if [[ ! -e ~/.fonts/Cica-Regular.ttf ]] ; then
-  wget https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
-  unzip Cica_*.zip
-  mkdir -p ~/.fonts  
-  mv Cica-*.ttf $HOME/.fonts
-  sudo fc-cache -fv  
-fi
-
-# tmux
-apt_install tmux
-link_file .tmux.conf
+# python
+apt_install python3-pip
+pip3 install neovim
 
 # Neovim
 apt_install software-properties-common xsel
@@ -156,6 +94,20 @@ if [[ ! -e ~/.cache/dein ]] ; then
 fi
 
 sudo update-alternatives --set editor /usr/bin/nvim || true
+sudo apt purge -y nano || true
+
+# Fonts
+if [[ ! -e ~/.fonts/Cica-Regular.ttf ]] ; then
+  wget https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_with_emoji.zip
+  unzip Cica_*.zip
+  mkdir -p ~/.fonts  
+  mv Cica-*.ttf $HOME/.fonts
+  sudo fc-cache -fv  
+fi
+
+# tmux
+apt_install tmux
+link_file .tmux.conf
 
 # peco
 if [[ ! -e $HOME/bin/peco ]] ; then
@@ -165,18 +117,6 @@ if [[ ! -e $HOME/bin/peco ]] ; then
   cp peco_linux_amd64/peco $HOME/bin
 fi
 link_file .config/peco
-
-# imwheel
-apt_install imwheel
-link_file .imwheelrc
-link_file .config/autostart/imwheel.desktop
-
-# # git config --global user.name ""
-# # git config --global user.email 
-if [ ! -e $HOME/.gitconfig ] ; then
-  wget https://gist.githubusercontent.com/anjn/650e64455b3d20bbf0d9e96138a4d000/raw/2e4817e53954954bf80bf1ebf7f99e2cb232fe18/.gitconfg
-  cp .gitconfg $HOME/.gitconfig
-fi
 
 # IME
 apt_install fcitx-mozc
@@ -190,8 +130,88 @@ if [ ! -e ~/.cache/enhancd ] ; then
 fi
 append_if_not_included $HOME/.bash_profile "source ~/.cache/enhancd/init.sh"
 
+## Regolith Linux
+if not_installed regolith-desktop-standard ; then
+  sudo add-apt-repository -y ppa:regolith-linux/release
+  sudo apt update
+  sudo apt install -y regolith-desktop-standard
+
+  #link_file .Xresources.d
+  #link_file .Xresources-regolith
+  #link_file .config/regolith
+
+  # Replace backend: https://github.com/chjj/compton/issues/152
+  #sudo sed -i 's/backend = "glx"/backend = "xrender"/' /etc/regolith/compton/config
+
+  apt_install i3xrocks-battery
+
+  echo "i3-wm.bar.position:	top" >> ~/.config/regolith/Xresources
+  echo "i3-wm.bar.trayoutput:	primary" >> ~/.config/regolith/Xresources
+  echo "i3-wm-mod: Mod1" >> ~/.config/regolith/Xresources
+  echo "i3-wm-alt: Mod4" >> ~/.config/regolith/Xresources
+fi
+
+# Google Chrome
+if not_installed google-chrome-stable ; then
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo apt install -y ./google-chrome-stable_current_amd64.deb
+fi
+
+#apt_install gnome-icon-theme
+apt_install tree
+apt_install build-essential valgrind
+apt_install htop keepass2 fio hddtemp smartmontools
+
+# Google drive
+if not_installed google-drive-ocamlfuse ; then
+  sudo add-apt-repository -y ppa:alessandro-strada/ppa
+  sudo apt update
+  sudo apt install -y google-drive-ocamlfuse
+fi
+
+if [[ ! -e ~/GoogleDrive ]] ; then
+  google-drive-ocamlfuse
+  mkdir ~/GoogleDrive
+fi
+#append_if_not_included $HOME/.bash_profile "google-drive-ocamlfuse GoogleDrive"
+
+# Linuxbrew
+#if not_installed linuxbrew-wrapper ; then
+#  sudo apt install -y linuxbrew-wrapper
+#  brew
+#  append_if_not_included $HOME/.bash_profile 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"'
+#  append_if_not_included $HOME/.bash_profile 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"'
+#  append_if_not_included $HOME/.bash_profile 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"'
+#fi
+
+if not_installed steam-launcher ; then
+  wget https://repo.steampowered.com/steam/archive/precise/steam_latest.deb
+  sudo apt install -y ./steam_latest.deb
+fi
+
+# imwheel
+#apt_install imwheel
+#link_file .imwheelrc
+#link_file .config/autostart/imwheel.desktop
+
+# # git config --global user.name ""
+# # git config --global user.email 
+if [ ! -e $HOME/.gitconfig ] ; then
+  wget https://gist.githubusercontent.com/anjn/650e64455b3d20bbf0d9e96138a4d000/raw/2e4817e53954954bf80bf1ebf7f99e2cb232fe18/.gitconfg
+  cp .gitconfg $HOME/.gitconfig
+fi
+
 # bashrc
 append_if_not_included $HOME/.bashrc "source $dir/home_ubuntu/.bashrc"
 
 # Vitis
 apt_install opencl-headers
+
+# nodejs
+if ! which n ; then
+  apt_install nodejs npm
+  sudo npm install -g n
+fi
+sudo n lts
+sudo apt purge -y nodejs npm || true
+sudo apt autoremove -y
